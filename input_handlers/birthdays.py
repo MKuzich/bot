f61d2688174ad13ac7e9619823e542f88708e1ea
Igor <aigor9534@gmail.com>
2024-03-16 15:05:44 +0200

    fix func | out data in table

--- input_handlers/birthdays.py
+++ input_handlers/birthdays.py
@@ -14,10 +14,15 @@ def birthdays(args, contacts):
     data_for_table = []
     for date, info in birthdays_contact.items():
         for day, name, phones, email, address in info:
+            phones = phones or '?'
+            email = email or '?'
+            address = address or '?'
+
             data_for_table.append([date, day, name, phones, email, address])
 
     if not data_for_table:
         raise NoBirthdays
 
+     #missingval do not working with maxcolwidths if one of element is None
     return "\n" + tabulate(data_for_table,
-                    headers=['Date', 'Day', 'Name', 'Phones', 'Email', 'Address'], tablefmt="grid", missingval="?") + "\n"
+                    headers=['Date', 'Day', 'Name', 'Phones', 'Email', 'Address'], tablefmt="grid", missingval="?", maxcolwidths=[None, 30, 30, 30, 30]) + "\n"
