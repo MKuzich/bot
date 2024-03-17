253796f81757cfde7e01880bb5018f1c60d9ae57
Oleh Solomko <solo.oleg1987@gmail.com>
2024-03-17 23:10:03 +0200

    newsorttag

--- input_handlers/sort_notes_by_tag.py
+++ input_handlers/sort_notes_by_tag.py
@@ -7,29 +7,26 @@ def sort_notes_by_tag(args, notes_manager):
     if args:
         tags = [arg.strip().lower() for arg in ' '.join(args).split(',')]  
     else:
-        raise NoteEmptyError
+        raise NoteEmptyError("No tags were provided.")
 
     sorted_notes = notes_manager.sort_notes_by_tag(tags)
     if not sorted_notes:
-        raise NoteSearchTagError
+        raise NoteSearchTagError("No notes found with those tags.")
 
     data_for_table = []
-    for tag in tags:
-        #data_for_table.append(f"Tag: {tag.capitalize()}")
-        for note in sorted_notes:
-            if tag.lower() in [t.lower() for t in note.tag]:
-                note_id = note.note_id
-                note_title = note.title or "?"
-                note_description = note.description or "?"
-                note_tag = ', '.join(note.tag) or "?"
-                note_date = note.date.strftime("%d.%m.%Y")
+    for note in sorted_notes:
+        note_id = note.note_id
+        note_title = note.title or "?"
+        note_description = note.description or "?"
+        note_tag = ', '.join(note.tag) or "?"
+        note_date = note.date.strftime("%d.%m.%Y")
 
-                data_for_table.append([note_id, note_title, note_description, note_tag, note_date])
+        data_for_table.append([note_id, note_title, note_description, note_tag, note_date])
 
-    #settings for tabulate
-    headers=['ID', 'Title', 'Description', 'Tags', 'Date']
-    tablefmt="grid"
-    missingval="?"
-    maxcolwidths=[None, 30, 30, 30, 30]
+    headers = ['ID', 'Title', 'Description', 'Tags', 'Date']
+    table_format = "grid"
+    missing_value = "?"
+    maxcolwidths = [None, 30, 30, 30, 30] 
 
-    return table_output(data_for_table, headers, tablefmt, missingval,  maxcolwidths)
+    return table_output(data_for_table, headers, table_format, missing_value, maxcolwidths)
+
