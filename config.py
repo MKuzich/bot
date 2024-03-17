ae9099330474da1a719ed54925fc0cb6f16a78f4
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-03-17 17:17:16 +0200

    add weather

---
+++ config.py
@@ -0,0 +1,3 @@
+IP_API = 'http://ip-api.com'
+WEATHER_API = 'https://api.openweathermap.org'
+WEATHER_API_KEY = 'ed975cf96ca5dbd92cd2a9f0b92c7f20'
---
+++ helpers/location.py
@@ -0,0 +1,9 @@
+"""Helpers function to get user location"""
+import requests
+from config import IP_API
+
+def get_location_info():
+    request = requests.get(f"{IP_API}/json/?fields=61439", timeout=5)
+    if request.status_code == 200:
+        return request.json()
+    return None
