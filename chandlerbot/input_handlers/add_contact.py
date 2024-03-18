93c46f90c456f22fd7d65f3f4fc6509334c8546e
Igor <aigor9534@gmail.com>
2024-03-18 13:52:25 +0200

    check length phone

--- chandlerbot/input_handlers/add_contact.py
+++ chandlerbot/input_handlers/add_contact.py
@@ -1,11 +1,12 @@
 from chandlerbot.input_error import input_error
 from chandlerbot.models import Record
 from chandlerbot.errors import NotValidPhoneNumber, NameIsString
+from chandlerbot.models.phone import Phone
 
 @input_error
 def add_contact(args, contacts):
     name, phone = args
-    if not phone.isdigit() or len(phone) != 10:
+    if not Phone(phone):
         raise NotValidPhoneNumber
     if not name.isalpha():
         raise NameIsString
